ec334b13452e349a28ea46307f6a0751779bebfd
Source Academy User <No public email provided>
2022-08-31 21:14:09 +0800

    Changes made from Source Academy

--- Studios/R4.js
+++ Studios/R4.js
@@ -10,14 +10,29 @@
 
 
 
-const x = 3;
-function fun(x) {
-    if (x % 2 === 0) {
-        const z = 20;
-    } 
-    else {
-        const z = 30;
-    }
-    return x + y + z;
-}
-const y = 5; const z = 10; fun(x + y);
+// const x = 3;
+// function fun(x) {
+//     if (x % 2 === 0) {
+//         const z = 20;
+//     } 
+//     else {
+//         const z = 30;
+//     }
+//     return x + y + z;
+// }
+// const y = 5;
+// const z = 10;
+// fun(x + y);
+
+
+// function fib(n) { if (n <= 1) { return n;
+// } else {
+// const x = fib(n - 2) + fib(n - 1); display(x);
+// return x;
+// } }
+// fib(5);
+
+function D(m, x) { display(m);
+return x; }
+    D(1, x => D(2, D(3, 3) * D(4, x))) (D(5, 5));
+    // same as (x => 3 * x)(5);
