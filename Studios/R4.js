cf0e99ab52a3548e45eb2a946fd67dc714139a1f
Source Academy User <No public email provided>
2022-08-31 18:35:12 +0800

    Changes made from Source Academy

--- Studios/R4.js
+++ Studios/R4.js
@@ -1,28 +1,23 @@
-// this index starts from 1
+// function f(w) {
+//     const x = 3;
+//     function g(x) { 
+//         return w + x;
+//     }
+//     return 2 * g(w); 
+// }
+// const w = 5;
+// f(w + 2);
 
 
-// 3a
-// const lst = list(7, 6, 5, 4, 3, 2, 1);
-function finding_4(lst,index){
-    return index === 1
-        ? head(lst)
-        : finding_4(tail(lst),index-1);
+
+const x = 3;
+function fun(x) {
+    if (x % 2 === 0) {
+        const z = 20;
+    } 
+    else {
+        const z = 30;
+    }
+    return x + y + z;
 }
-// display(finding_4(lst,4));
-
-
-// 3b
-
-// const lst = list(list(7), list(6, 5, 4), list(3, 2), 1);
-function finding_4_again(lst){
-    return finding_4(finding_4(lst,2),3);
-}
-// display(finding_4_again(lst));
-
-
-// 3c
-// too complex???
-const lst = list(7, list(list(list(6, 5, list(list(4)), 3), 2)), 1);
-function haiz(lst){
-    return finding_4(finding_4(lst,2),3);
-}
+const y = 5; const z = 10; fun(x + y);
